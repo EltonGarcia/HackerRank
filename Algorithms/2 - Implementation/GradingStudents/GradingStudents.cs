45164bc0c4375abfbe6e8df068a51f258a188b2e
Elton Garcia de Santana <elton_garcia@hotmail.com>
2017-09-16 00:02:06 -0300

    Algorithms > Implementation > Grading Students

--- Algorithms/2 - Implementation/GradingStudents/GradingStudents.cs
+++ Algorithms/2 - Implementation/GradingStudents/GradingStudents.cs
@@ -4,5 +4,21 @@ namespace HackerRank.Algorithms.Implementation.GradingStudents
 {
     public class GradingStudents
     {
+        public static int[] RoundGrade(int[] grades)
+        {
+    		int factor = 5, acceptDifference = 3;
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                var grade = grades[i];
+
+                if (grade >= 38 && grade % factor >= acceptDifference)
+				{
+                    grades[i] = grade + (factor - (grade % factor));
+				}
+            }
+
+            return grades;
+        }
     }
 }
--- Algorithms/2 - Implementation/GradingStudents/GradingStudentsTests.cs
+++ Algorithms/2 - Implementation/GradingStudents/GradingStudentsTests.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Linq;
 using Xunit;
 
 namespace HackerRank.Algorithms.Implementation.GradingStudents
 {
     public class GradingStudentsTests
     {
-		
+		[Fact]
+		public void Main()
+		{
+			var test = new int[] { 73, 67, 38, 33 };
+            var expected = new int[] { 75, 67, 40, 33 };
+
+            var result = GradingStudents.RoundGrade(test);
+
+            Assert.True(Enumerable.SequenceEqual(expected, result));
+		}
     }
 }
