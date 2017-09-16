b7f7f0ea9cff0f2accf05bace02f2b5cc976e097
Elton Garcia de Santana <elton_garcia@hotmail.com>
2017-09-16 11:47:06 -0300

    Algorithms > Implementation > Apple and Orange

---
+++ Algorithms/2 - Implementation/AppleAndOrange/AppleAndOrange.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank.Algorithms.Implementation.AppleAndOrange
+{
+    public class AppleAndOrange
+    {
+        public static void CountItensInArea(int s, int t,
+                                            int appleTree, int orangeTree,
+                                            int[] apples, int[] oranges,
+                                            out int applesCount, out int orangesCount)
+        {
+            Func<int, bool> inArea = pos => pos >= s && pos <= t;
+
+            applesCount = apples.Select(pos => appleTree + pos).Count(inArea);
+            orangesCount = oranges.Select(pos => orangeTree + pos).Count(inArea);
+        }
+    }
+}
---
+++ Algorithms/2 - Implementation/AppleAndOrange/AppleAndOrangeTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+
+namespace HackerRank.Algorithms.Implementation.AppleAndOrange
+{
+    public class AppleAndOrangeTests
+    {
+        [Fact]
+        public void Main()
+        {
+            int s = 7, t = 11;
+            int appleTree = 5, orangeTree = 15;
+
+            var apples = new int[] { -2, 2, 1 };
+            var oranges = new int[] { 5, -6 };
+
+            var expectedApplesCount = 1;
+            var expectedOrangesCount = 1;
+
+            int applesCount, orangesCount;
+            AppleAndOrange.CountItensInArea
+            (
+                s, t,
+                appleTree, orangeTree,
+                apples, oranges, out applesCount, out orangesCount
+            );
+
+            Assert.Equal(expectedApplesCount, applesCount);
+            Assert.Equal(expectedOrangesCount, orangesCount);
+        }
+
+    }
+}
