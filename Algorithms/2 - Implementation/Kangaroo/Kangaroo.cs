867eb4be3e62413f18011c574a7d0d0418c7e337
Elton Garcia de Santana <elton_garcia@hotmail.com>
2017-09-19 22:37:40 -0300

    Algorithms > Implementation > Kangaroo

---
+++ Algorithms/2 - Implementation/Kangaroo/Kangaroo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HackerRank.Algorithms.Implementation.Kangaroo
+{
+    public class Kangaroo
+    {
+        public static bool CheckCollision(int x1, int v1, int x2, int v2)
+        {
+            return (v1 > v2 && ((x2 - x1) % (v1 - v2) == 0));
+        }
+    }
+}
---
+++ Algorithms/2 - Implementation/Kangaroo/KangarooTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+
+namespace HackerRank.Algorithms.Implementation.Kangaroo
+{
+    public class KangarooTests
+    {
+        [Fact]
+        public void Collision()
+        {
+            int x1 = 0, v1 = 3;
+            int x2 = 4, v2 = 2;
+
+            var result = Kangaroo.CheckCollision(x1, v1, x2, v2);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void NoCollision()
+        {
+            int x1 = 0, v1 = 2;
+            int x2 = 5, v2 = 3;
+
+            var result = Kangaroo.CheckCollision(x1, v1, x2, v2);
+
+            Assert.False(result);
+        }
+    }
+}
