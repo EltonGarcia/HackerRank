c62751d42f899915c06ae9f5fc0eec32730dc44d
Elton Santana <elton.garcia.s@gmail.com>
2020-05-23 21:01:51 -0300

    Algorithms > Implementation > Between Two Sets

---
+++ Algorithms/2 - Implementation/BetweenTwoSets/BetweenTwoSets.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank.Algorithms.Implementation.BetweenTwoSets
+{
+    public class BetweenTwoSets
+    {
+        private int GCD(int a, int b)
+        {
+            while (a > 0 && b > 0)
+            {
+                if (a >= b)
+                {
+                    a = a % b;
+                }
+                else
+                {
+                    b = b % a;
+                }
+            }
+            return a + b;
+        }
+
+        private int LCM(int a, int b)
+        {
+            return (a * GCD(a, b)) * b;
+        }
+
+        public static int CountElementsBetweenSets(int[] a, int[] b)
+        {
+            var min = a.Max();
+            var max = b.Min();
+            var count = 0;
+
+            var i = min;
+            while (i <= max)
+            {
+                var factorOfA = true;
+                for (int j = 0; j < a.Length && factorOfA; j++)
+                {
+                    factorOfA &= i % a[j] == 0;
+                }
+                if (factorOfA)
+                {
+                    var factorOfB = true;
+                    for (int k = 0; k < b.Length && factorOfB; k++)
+                    {
+                        factorOfB &= b[k] % i == 0;
+                    }
+                    if (factorOfB)
+                    {
+                        count++;
+                    }
+                }
+
+                i++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Algorithms/2 - Implementation/BetweenTwoSets/BetweenTwoSetsTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace HackerRank.Algorithms.Implementation.BetweenTwoSets
+{
+    public class BetweenTwoSetsTests
+    {
+		[Fact]
+		public void Main()
+		{
+			var A = new int[] { 2, 4 };
+			var B = new int[] { 16, 32, 96};
+
+			var expected = 3;
+            var result = BetweenTwoSets.CountElementsBetweenSets(A, B);
+
+			Assert.Equal(expected, result);
+		}
+    }
+}
