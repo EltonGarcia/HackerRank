8d083d8f54d69d54aa135796690cf9bd66363827
Elton Santana <elton.garcia.s@gmail.com>
2020-06-22 10:35:57 -0300

    Functional Programming > Introduction > Filter Array > Solution

--- FilterArray.fsx
+++ FilterArray.fsx
@@ -11,3 +11,24 @@ The purpose of this challenge is to learn how to write your own implementation o
 
 - Explanation: 2, 1 and 0 are the list elements that are less than the X delimiter, 3. They are displayed in the same order as they were in the original list.
 *)
+open System
+
+let rec filter (list: List<int>, n: int, result: List<int>) = 
+    match list with
+    | [] -> result |> List.rev
+    | y::ys -> 
+        if (y < n) 
+        then filter (ys, n, y::result) 
+        else filter (ys, n, result)
+
+[<EntryPoint>]
+let main argv = 
+    let n = Console.ReadLine() |> int;
+    let list = 
+        Seq.initInfinite (fun _ -> Console.ReadLine())
+        |> Seq.takeWhile(String.IsNullOrEmpty >> not)
+        |> Seq.map System.Int32.Parse
+        |> Seq.toList
+
+    List.iter (printfn("%d")) (filter (list, n, []))
+    0
