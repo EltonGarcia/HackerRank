7c695ea7168d396b8d60f7c0b4eee47688975ef7
Elton Santana <elton.garcia.s@gmail.com>
2020-06-23 10:48:02 -0300

    Functional > Programming Introduction > Filter Positions in a List (Refactory)

--- FunctionalProgramming/FilterListPosition.fsx
+++ FunctionalProgramming/FilterListPosition.fsx
@@ -1,11 +1,15 @@
 open System
 
+let rec filterList list =
+    match list with
+    | _::x::xs -> x::filterList xs
+    | _ -> []
+
 [<EntryPoint>]
 let main argv =
     let list =
-        Seq.initInfinite(fun index -> (index, Console.ReadLine()))
-        |> Seq.takeWhile((fun (index, item) -> String.IsNullOrEmpty(item)) >> not)
-        |> Seq.filter(fun (index, item) -> index % 2 <> 0)
-        |> Seq.map(fun (index, item) -> item)
-    Seq.iter (printfn("%s")) (list)
+        Seq.initInfinite(fun _ -> Console.ReadLine())
+        |> Seq.takeWhile(String.IsNullOrEmpty >> not)
+        |> Seq.toList
+    List.iter (printfn("%s")) (filterList list)
     0
